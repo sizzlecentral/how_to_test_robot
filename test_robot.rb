f9f98cb78d03c61463fb08b569578e3f2ebacb95
Don Cooper <don_cooper@hotmail.com>
2017-06-22 09:56:01 -0400

    Test 7 clear

--- test_robot.rb
+++ test_robot.rb
@@ -89,12 +89,17 @@ class TestRobot < MiniTest::Test
   end
 
   def test_workday_on_day_off_returns_true
-    skip
+
     # arrange
+    hal = Robot.new
+    today = (hal.day_off == false)
 
     # act
+    working = hal.workday?(today)
 
     # assert
+    assert_equal(working, true)
+
   end
 
   def test_workday_not_day_off_returns_false
